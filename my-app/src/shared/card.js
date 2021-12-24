3dc60590a2b5b5e63fb83f568726972228783d5c
Himanshu Sah <himanshusah41@gmail.com>
2021-12-24 10:30:28 +0545

    Added reverse to the styling of Card component

--- my-app/src/shared/card.js
+++ my-app/src/shared/card.js
@@ -1,9 +1,21 @@
-function card({children}){
+import Proptypes from 'prop-types';
+
+function card({children,reverse}){
     return(
-        <div className="card">
+        <div className="card"
+        style = {{backgroundColor :  reverse ? 'rgb(0,0,0,0.4)':'#f5f5f5'}}>
             {children}
         </div>
     )
 }
+
+card.defaultProps = {
+    reverse : false,
+};
+
+card.propTypes = {
+    children : Proptypes.node.isRequired,
+    reverse : Proptypes.bool,
+};
 
 export default card;
